8399e6cacc7e83fbddbc3a699a2b1536ff4c463d
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2024-06-24 10:52:45 +0300

    add project

---
+++ SimpleASPNETProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleASPNETProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
